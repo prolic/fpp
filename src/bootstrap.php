ace48df24edc771b11fa4a4727f5b5bf4398edb4
prolic <saschaprolic@googlemail.com>
2018-03-08 14:41:04 +0800

    update fpp bin
    resolves https://github.com/prolic/fpp/issues/17

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -11,7 +11,17 @@ declare(strict_types=1);
 
 namespace Fpp;
 
-require __DIR__ . '/../vendor/autoload.php';
+$autoloadFiles = [
+    __DIR__ . '/../vendor/autoload.php',
+    __DIR__ . '/../../../autoload.php',
+];
+
+foreach ($autoloadFiles as $autoloadFile) {
+    if (file_exists($autoloadFile)) {
+        require_once $autoloadFile;
+        break;
+    }
+}
 
 $files = [
     'dump.php',
