dd0265a5c7d65585ee17dcd516b15d329e7c390b
prolic <saschaprolic@googlemail.com>
2018-03-09 16:40:06 +0800

    clean up

--- src/ParseError.php
+++ src/ParseError.php
@@ -25,18 +25,6 @@ class ParseError extends \RuntimeException
         ));
     }
 
-    public static function lowerCaseDefinitionName(array $actual, string $filename): ParseError
-    {
-        $filePart = empty($filename) ? '' : ' on file \'' . $filename . '\'';
-
-        return new self(sprintf(
-            'Syntax error, definiton name %s must be upper case at line %d%s',
-            $actual[1],
-            $actual[2],
-            $filePart
-        ));
-    }
-
     public static function unexpectedTokenFound(string $expected, array $actual, string $filename): ParseError
     {
         $filePart = empty($filename) ? '' : ' on file \'' . $filename . '\'';
