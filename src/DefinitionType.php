381b2847288e3c64d6632790e4331928ce0eb82e
Gildas Quéméner <gildas.quemener@gmail.com>
2018-07-27 21:25:07 +0200

    Removed exception definition type

--- src/DefinitionType.php
+++ src/DefinitionType.php
@@ -16,13 +16,11 @@ final class DefinitionType
 {
     public const OPTIONS = [
         'Data' => 0,
-        'Exception' => 1,
-        'Marker' => 2,
+        'Marker' => 1,
     ];
 
     public const Data = 0;
-    public const Exception = 1;
-    public const Marker = 2;
+    public const Marker = 1;
 
     private $name;
     private $value;
@@ -36,11 +34,6 @@ final class DefinitionType
     public static function data(): self
     {
         return new self('Data');
-    }
-
-    public static function exception(): self
-    {
-        return new self('Exception');
     }
 
     public static function marker(): self
