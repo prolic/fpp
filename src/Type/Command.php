93b509ff9bc34bc52ac9b6af5291ec48cc70b7f1
prolic <saschaprolic@googlemail.com>
2021-01-06 13:20:39 -0300

    update copyright

--- src/Type/Command.php
+++ src/Type/Command.php
@@ -2,7 +2,7 @@
 
 /**
  * This file is part of prolic/fpp.
- * (c) 2018-2020 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ * (c) 2018-2021 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
